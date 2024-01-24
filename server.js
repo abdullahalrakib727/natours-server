9ed7f6abba8596af9859624e6e8ee912a4e9b888
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-25 01:45:45 +0600

    added mongoose

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("mongodb connected properly");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const port = process.env.PORT || 3000;
 
