e4caad25bb7677472f83393019094acfb1c273b6
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-25 03:28:28 +0600

    added tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,37 +1,70 @@
 const Tour = require("../models/tourModel");
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: { tours },
-  });
+// sending all tours
+
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    return res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (error) {
+    return res.status(404).json({ status: "failed", message: error.message });
+  }
 };
+
+// creating new tour
 
 const createTour = async (req, res) => {
- await Tour.create({});
+  try {
+    const newTour = await Tour.create(req.body);
 
-  res.status(201).json({
-    status: "success",
-    // data: { newTour },
-  });
+    return res.status(201).json({
+      status: "success",
+      data: { tour: newTour },
+    });
+  } catch (error) {
+    return res.status(400).json({ status: "failed", message: error.message });
+  }
 };
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((tour) => tour.id === id);
-  res.status(200).send({
-    status: "success",
-    data: tour,
-  });
+// sending sing tour by id
+
+const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id:req.params.id})
+
+    return res.status(200).send({
+      status: "success",
+      data: tour,
+    });
+  } catch (error) {
+    return res.status(404).json({ status: "failed", message: error.message });
+  }
 };
 
-const updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "will update here",
-  });
+// updating tour by id
+
+const updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json({
+      status: "success",
+      data: updatedTour,
+    });
+  } catch (error) {
+    return res.status(400).json({ status: "failed", message: error.message });
+  }
 };
+
+// deleting tour by id
 
 const deleteTour = (req, res) => {
   res.status(204).json({
