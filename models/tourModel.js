70cda28e7553d2aa3ab092db892940991a20aedb
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-01 04:06:39 +0600

    added validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -19,10 +19,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum:{
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult"
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingQuantity: {
       type: Number,
@@ -64,6 +70,11 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// document middleware : runs before .save( ) and .create(). not insertMany()
+// tourSchema.pre("save", function () {
+//   console.log(this);
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
