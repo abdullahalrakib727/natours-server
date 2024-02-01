76c9019904333fba4b02acbdfc4dc85373382a65
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-01 06:06:23 +0600

    added new error handeling cls

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
 
 const app = express();
 
@@ -24,11 +25,29 @@ app.use("/api/v1/tours", tourRouter);
 // users
 app.use("/api/v1/users", userRouter);
 
+// api not found
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// error handleling middleware
+
+
+app.use((err,req,res,next)=>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+})
 
 module.exports = app;
